0952c0ddb1666626f2a5871758e5a7076a742874
Christophe Mertens <christophe.mertens@ulb.be>
2023-06-15 13:09:49 +0200

    hello

---
+++ block_documentation.php
@@ -0,0 +1,19 @@
+<?php
+class block_documentation extends block_base {
+    public function init() {
+        $this->title = get_string('documentation', 'block_documentation');
+    }
+
+    public function get_content() {
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content         =  new stdClass;
+        $this->content->text   = 'The content of our Documentation block!';
+        $this->content->footer = 'Footer here...';
+
+        return $this->content;
+    }
+
+}
