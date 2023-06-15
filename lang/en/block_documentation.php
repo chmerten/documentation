e724352d2413dfdcb5c50af80b70c22d93835e2d
Christophe Mertens <christophe.mertens@ulb.be>
2023-06-15 13:19:43 +0200

    hello

---
+++ lang/en/block_documentation.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Documentation block';
+$string['documentation'] = 'Documentation CAP';
+$string['documentation:addinstance'] = 'Add a new Documentation block';
+$string['documentation:myaddinstance'] = 'Add a new Documentation block to the My Moodle page';
