107c3c894b0df5ac00f5c020df0e251119682f25
Christophe Mertens <christophe.mertens@ulb.be>
2023-08-22 10:41:33 +0200

    hello

--- block_documentation.php
+++ block_documentation.php
@@ -8,13 +8,19 @@ class block_documentation extends block_base {
     public function get_content() {
 
         global $DB;
+        global $CFG;
         // Get visible category courses
-        $sql = "SELECT c.id, c.fullname, cc.name
+        $sql = "SELECT c.id, c.fullname
                         FROM {course} c
                         JOIN {course_categories} cc ON c.category = cc.id
                         WHERE cc.idnumber = :categoryidnumber AND c.visible = 1
                         ORDER BY c.sortorder ASC";
         $course_list = $DB->get_records_sql_menu($sql, array('categoryidnumber' => 'TESTCM'));
+
+        $course_list_text='';
+        foreach ($course_list as $c) {
+            $course_list_text = $course_list_text . '<a href="https://www.google.com">'. $CFG->wwwroot . '</a>'
+        }
 
 
         if ($this->content !== null) {
