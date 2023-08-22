13c86f7edcfddf4ad47931ac2309f18f5dfc1f1b
Christophe Mertens <christophe.mertens@ulb.be>
2023-08-22 10:58:48 +0200

    hello

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'block_documentation';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2011062808;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2011062809;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
